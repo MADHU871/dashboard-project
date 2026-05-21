9cd967c35cbde83b8cc5a55eb6ce691138a0da66
MADHU871 <mad0008271@gmail.com>
2026-05-21 15:54:55 +0530

    Added Docker setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>DevOps Dashboard</h1>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3,1fr)",
+          gap: "20px",
+        }}
+      >
+        <div style={{ background: "#eee", padding: "20px" }}>
+          <h2>Users</h2>
+          <p>1200</p>
+        </div>
+
+        <div style={{ background: "#eee", padding: "20px" }}>
+          <h2>Revenue</h2>
+          <p>$54,000</p>
+        </div>
+
+        <div style={{ background: "#eee", padding: "20px" }}>
+          <h2>Orders</h2>
+          <p>340</p>
+        </div>
+      </div>
     </div>
   );
 }
